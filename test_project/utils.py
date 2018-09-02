d5b9839c4ef876e22aa7943de0c88454f2088c6b
Leon <leon@Leons-MacBook-Pro.local>
2018-09-03 00:12:51 +0200

    test

--- test_project/utils.py
+++ test_project/utils.py
@@ -80,7 +80,7 @@ def send_confirm_email(payload, email):
 
 
 def send_reset_password_email(payload, email):
-    email_token = jwt.encode({'payload': payload, 'exp': time.time() + 10 * 60}, CONSTANTS.get('JWT_SECRET', 'hello'), algorithm='HS256')
+    #email_token = jwt.encode({'payload': payload, 'exp': time.time() + 10 * 60}, CONSTANTS.get('JWT_SECRET', 'hello'), algorithm='HS256')
 
     try:
 
